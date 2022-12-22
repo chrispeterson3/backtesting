ac9342d126b3c2b8579bb1e64585c9c18b96c1ae
Chris Peterson <peterson.m.chris@gmail.com>
2022-12-22 15:09:05 -0800

    Cleanup

--- strategies/smallCapGappers/orchestrateStrategy.ts
+++ strategies/smallCapGappers/orchestrateStrategy.ts
@@ -12,7 +12,7 @@ import { strategyName } from "./mod.ts";
 import { resultsMapper } from "./resultsMapper.ts";
 import { FilteredStrategyResult } from "./types.ts";
 
-const { from, to } = { from: "2022-12-19", to: "2022-12-21" };
+const { from, to } = { from: "2022-12-19", to: "2022-12-31" };
 
 export async function orchestrateStrategy(): Promise<
   Array<FilteredStrategyResult>
--- strategy/mod.ts
+++ strategy/mod.ts
@@ -1,5 +1,3 @@
-export * from "./getStrategyBars.ts";
-export * from "./getFilteredStrategyResults.ts";
 export * from "./getPriceAction.ts";
 export * from "./getChartData.ts";
 export * from "./createCharts.ts";
